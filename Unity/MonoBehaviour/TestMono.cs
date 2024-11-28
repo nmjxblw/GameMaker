2da02e436bad07c91502b075c68a734049426438
z c <915253492@qq.com>
2024-11-28 16:29:17 +0800

    测试

---
+++ Unity/MonoBehaviour/TestMono.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GameMaker
+{
+	public class TestMono: MonoBehaviour
+	{
+		void Awake() {
+			print("Test");
+		}
+	}
+}
---
+++ Unity/ScriptableObject/TestSO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace GameMaker
+{
+	[CreateAssetMenu(fileName = "TestSO", menuName = "GameMaker")]
+	public class TestSO : ScriptableObject, IComparable<TestSO>
+	{
+		[SerializeField]
+		private TextAsset m_TextAsset;
+
+		public int CompareTo(TestSO other)
+		{
+			return this.name.CompareTo(other.name);
+		}
+
+		void OnEnable()
+		{
+			string s = m_TextAsset.text;
+			Debug.Log($"s={s}");
+		}
+	}
+}
+namespace GameMaker.Unity.Test
+{
+	public class Config
+	{
+
+	}
+}
